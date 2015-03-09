128a190e065efd37c6a38785f859f270c1fa0233
Anthony Zotti <azotti1337@gmail.com>
2015-03-09 12:26:47 -0700

    Filter tweets by location

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 var Twitter = require('twitter');
 var APIkeys = require('./APIkeys.js');
- 
+var SF = '-122.75,36.8,-121.75,37.8';
+
 var client = new Twitter({
   consumer_key: APIkeys.CONSUMER_KEY,
   consumer_secret: APIkeys.CONSUMER_SECRET,
@@ -8,7 +9,7 @@ var client = new Twitter({
   access_token_secret: APIkeys.ACCESS_TOKEN_SECRET
 });
 
-client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+client.stream('statuses/filter', {locations: SF}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
