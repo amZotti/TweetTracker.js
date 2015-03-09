f450d91b5c111a6596f73cbf4b4e4bef6cd268bd
Anthony Zotti <azotti1337@gmail.com>
2015-03-09 13:26:59 -0700

    Rename 'client' as 'TwitterClient'

--- twitterClient.js
+++ twitterClient.js
@@ -3,14 +3,14 @@ var APIkeys = require('./APIkeys.js');
 var SF = '-122.75,36.8,-121.75,37.8';
 var keywords = [/javascript/, /ruby/, /san/];
 
-var client = new Twitter({
+var TwitterClient = new Twitter({
   consumer_key: APIkeys.CONSUMER_KEY,
   consumer_secret: APIkeys.CONSUMER_SECRET,
   access_token_key: APIkeys.ACCESS_TOKEN,
   access_token_secret: APIkeys.ACCESS_TOKEN_SECRET
 });
 
-client.stream('statuses/filter', {locations: SF}, function(stream) {
+TwitterClient.stream('statuses/filter', {locations: SF}, function(stream) {
   stream.on('data', function(tweet) {
     if (tweetContainsKeywords(tweet))
       console.log(tweet.text);
