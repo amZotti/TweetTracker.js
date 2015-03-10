adea63ac9e9cb8db8a23174bd1a819920ebd603b
Anthony Zotti <azotti1337@gmail.com>
2015-03-10 11:23:51 -0700

    Angular mainController fetches keywords from server

--- public/javascripts/mainController.js
+++ public/javascripts/mainController.js
@@ -1,1 +1,9 @@
-console.log('works');
+var app = angular.module('app', []);
+
+app.controller('mainController', function($scope, $http) {
+  $scope.getData = function() { 
+    $http.get("/api/data").then(function(data) {
+      console.log(data);
+    });
+  }
+});
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var grapher = require('../services/graph/restfulGraphClient.js');
-var criteria = require('../tweetDetectionCriteria.js');
-var helper = require('../services/listen/helpers.js');
+var grapher = require("../services/graph/restfulGraphClient.js");
+var criteria = require("../tweetDetectionCriteria.js");
+var helper = require("../services/listen/helpers.js");
 
-router.get('/', function(req, res, next) {
-  var city = criteria.searchCity;
-  var keyword = criteria.searchKeyword;
-  var keywords = helper.regexesToStrings(criteria.detectKeywords);
+var city = criteria.searchCity;
+var keyword = criteria.searchKeyword;
+var keywords = helper.regexesToStrings(criteria.detectKeywords);
+
+
+router.get("/", function(req, res, next) {
   console.log("Updating Graph: ");
   grapher.graphTweets();
-  res.render('index', {city: city, keyword: keyword, keywords: keywords});
+  res.render("index", {city: city, keyword: keyword, keywords: keywords});
+});
+
+router.get("/api/data", function(req, res) {
+  res.send({city: city, keyword: keyword, keywords: keywords});
 });
 
 module.exports = router;
