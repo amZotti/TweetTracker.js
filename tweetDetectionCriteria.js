abb75623e2837e2225a63ae5cffc51332f4cc664
Anthony Zotti <azotti1337@gmail.com>
2015-03-10 15:02:25 -0700

    Place graph information into middle of index

--- tweetDetectionCriteria.js
+++ tweetDetectionCriteria.js
@@ -2,7 +2,7 @@ module.exports = {
   searchCity: 'San Francisco',
   searchKeyword: 'hi',
 
-  detectKeywords:  [/javascript/, /ruby/, /club/, /facebook/, /google/, /cats/, /hi/, /high/, /beach/, /diesel/, /obama/, /america/],
+  detectKeywords:  [/javascript/, /ruby/, /club/, /facebook/, /google/, /cats/, /hi/, /high/, /beach/, /diesel/, /obama/, /america/, /baller/, /love/, /lol/, /cops/],
 
   detectCity: {coords: "-123,36,-121,38", name: 'San Francisco'}
 };
