ef9e8190f8c20362529acdf261ce3ee026981af4
Anthony Zotti <azotti1337@gmail.com>
2015-03-10 09:19:27 -0700

    Display city and search keyword of graph

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var grapher = require('../services/graph/restfulGraphClient.js');
+var criteria = require('../tweetDetectionCriteria.js');
 
 router.get('/', function(req, res, next) {
   console.log("Updating Graph: ");
   grapher.graphTweets();
-  res.render('index', { title: 'Express' });
+  res.render('index', {city: criteria.searchCity, keyword: criteria.searchKeyword});
 });
 
 module.exports = router;
